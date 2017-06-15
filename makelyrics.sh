348cf328c0eeda065d371ca7954c05d37b81d184
Iikka Hauhio <iikka@kaivos.org>
2017-06-15 18:05:53 +0300

    added possibility to add a title

--- makelyrics.sh
+++ makelyrics.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-MIDI=$1
-MUSIC=$2
-OUTPUT=$3
+TITLE=$1
+MIDI=$2
+MUSIC=$3
+OUTPUT=$4
 TEMP=`mktemp -d`
 
-röda videolyrics.röd "$MIDI" 24 "$TEMP/frame#" 2>/dev/null
+röda videolyrics.röd "$MIDI" "$TITLE" 24 "$TEMP/frame#" 2>/dev/null
 ffmpeg -framerate 24 -start_number 1 -i "$TEMP/frame%d.png" -i "$MUSIC" "$OUTPUT"
 rm -r "$TEMP"
